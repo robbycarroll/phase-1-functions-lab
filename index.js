bb472e443c6863c6258b282ee432376259bc58e3
Robert Carroll <robertcarrollsales@gmail.com>
2022-02-01 21:21:13 -0500

    commit tree dc4a7072d28bf8202c40d703db4fb3735f3098ee

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+return Math.abs(42 - destination) 
+}
+
+function distanceFromHqInFeet(destination) {
+    return distanceFromHqInBlocks(destination) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice (start, end) {
+const distance = distanceTravelledInFeet(start, end);
+if (distance < 400) return 0 
+if (distance > 2500) return 'cannot travel that far'
+if (distance < 2000) return (distance -400) * .02
+if (distance > 2000) return 25
+}
